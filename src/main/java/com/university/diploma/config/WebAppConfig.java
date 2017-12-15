30cd8a2094d72b08c2bdaf741d1a831eee96ae69
DimaMyna <3921555Op>
2017-12-16 01:40:42 +0200

    add package

--- src/main/java/com/university/diploma/config/WebAppConfig.java
+++ src/main/java/com/university/diploma/config/WebAppConfig.java
@@ -3,6 +3,8 @@ package com.university.diploma.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -19,9 +21,7 @@ public class WebAppConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//        registry.addResourceHandler("/webapp/**").addResourceLocations("/");
-        registry.addResourceHandler("/static/**").addResourceLocations("/static/");
-//        registry.addResourceHandler("/webapp/view/**").addResourceLocations("/view/");
+        registry.addResourceHandler("/WEB-INF/static/**").addResourceLocations("/static/");
 //        registry.addResourceHandler("/js/**").addResourceLocations("/js/");
 //        registry.addResourceHandler("/css/**").addResourceLocations("/css/");
 //        registry.addResourceHandler("/image/**").addResourceLocations("/image/");
